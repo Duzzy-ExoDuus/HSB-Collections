ee15229d26ca4fead9d3a33f9bcd008b9059660f
Johnny SB Hyeon <sbhyeon@hotmail.com>
2022-04-09 21:41:35 +0900

    second commit

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+sudo cp -r ./backgrounds/HSB-Collections /usr/share/backgrounds/
+sudo cp -r ./fonts/HSB-collection /usr/share/fonts/truetype/
+sudo cp -r ./grub_themes/dedsec /boot/grub/themes/
+sudo cp -r ./icons/HSB-Collection /usr/share/icons/
+sudo cp -r ./plymouth_themes/dotLockG /usr/share/plymouth/themes/
+sudo cp -r ./themes/HSB-Collection /usr/share/themes/
+
+cat <<EOF
+# [GRUB]
+# Edit /etc/default/grub file. 
+# Find the line starting with "#GRUB_THEME" and change it to 
+# "GRUB_THEME=/boot/grub/themes/[YOURTHEME]/theme.txt" 
+# Run sudo update-grub
+#
+# [Plymouth]
+# sudo plymouth-set-default-theme [YOURTHEME] -R
+EOF
